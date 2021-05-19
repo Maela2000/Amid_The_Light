bd1a25197696088726b7c904e82dbb66ff99b1e2
Tsuki <maela.le-gall@e-artsup.net>
2021-05-19 14:22:47 +0200

    onesperequecestlabonne

---
+++ Assets/Scripts/EnnemyTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemyTest : MonoBehaviour
+{
+    public float Enemyspeed = 0.3f;
+    private Transform target;
+
+    public Transform Target1 { get => target; set => target = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowTarget();
+    }
+
+    private void FollowTarget()
+    {
+        if (target != null)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, Enemyspeed * Time.deltaTime);//The enemy follow the player
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player") //the enemy follow the player when the player enter on the collider's enemy
+        {
+            target = collision.transform;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)//the enemy stop follow the player when the player exit on the collider's enemy
+    {
+        if (collision.tag == "Player")
+        {
+            target = null;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed;
+    public SpriteRenderer sprite;
+
+    [SerializeField]
+    private Rigidbody2D rigidbody2d;
+    [SerializeField]
+    private bool IsGrounded = false;
+    [SerializeField]
+    private float jumpVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+        if (IsGrounded == true && (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space)))
+        {
+
+            rigidbody2d.velocity = Vector2.up * jumpVelocity;
+            Debug.Log("Jump");
+            //animator.SetBool("IsGrounded", false);
+            IsGrounded = false;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "ground")
+        {
+            IsGrounded = true;
+            Debug.Log("IsGrounded");
+            //animator.SetBool("IsGrounded", true);
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Wall" && Input.GetKey("x"))
+        {
+            Physics2D.IgnoreLayerCollision(8, 9, true);
+            Physics2D.IgnoreLayerCollision(8, 10, true);
+            sprite.color = new Color(0, 0, 0, 1);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            Physics2D.IgnoreLayerCollision(8, 9, false);
+            Physics2D.IgnoreLayerCollision(8, 10, false);
+            sprite.color = new Color(1, 1, 1, 1);
+        }
+    }
+}
