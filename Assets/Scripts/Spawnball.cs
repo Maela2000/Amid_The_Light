b5d8070e996d2ea89e0024e492284023cd05e308
Eugeniov <bastien.coquelin@gmail.com>
2021-06-03 13:50:41 +0200

    yo

--- Assets/Scripts/Spawnball.cs
+++ Assets/Scripts/Spawnball.cs
@@ -8,7 +8,7 @@ public class Spawnball : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(gameObject);
+        Instantiate(gameObject, 3f);
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/lampadaire.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lampadaire : MonoBehaviour
+{
+    private float x;
+    private bool up = true;
+    private bool down= false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(x);
+        if (x <= 89 && x >= 0 && (up == true))
+        {
+            transform.Rotate(0, 0, 1f);
+            x = x + 1;
+            if (x >= 90)
+            {
+                up = false;
+                down = true;
+            }
+        }
+
+        else if (x <= 90 && x >= 1 && (down == true))
+            {
+                transform.Rotate(0, 0, -1f);
+                x = x - 1;
+            if (x <= 0)
+            {
+                up = true;
+                down = false;
+            }
+        }
+
+    }
+}
