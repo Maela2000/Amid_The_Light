f49eb19066461605657547912b3b7fcca749bcff
Eugeniov <bastien.coquelin@gmail.com>
2021-06-25 17:36:46 +0200

    envoir tout

--- Assets/Scripts/Camboss.cs
+++ Assets/Scripts/Camboss.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class Camboss : MonoBehaviour
 {
     public Transform player;
+    public Vector3 offset;
+
+    public float posXMax;
+    public float posXMin;
+    public float posYMax;
+    public float posYMin;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +21,9 @@ public class Camboss : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (player.position.y >= posYMax || player.position.y <= posYMin)
+        {
+            transform.position = new Vector3(offset.x, player.position.y, offset.z);
+        }
     }
 }
